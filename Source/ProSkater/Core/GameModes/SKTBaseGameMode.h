515c641875fa30b4b3aea00f46814a9bf90ea36d
Maçal Gabriel <macal741@live.com>
2025-08-16 12:20:01 -0300

    +create base game mode

---
+++ Source/ProSkater/Core/GameModes/SKTBaseGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "SKTBaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROSKATER_API ASKTBaseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
